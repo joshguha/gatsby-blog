a5f49c99b7c410ebb5fb894b4f8170c90577449f
Josh Guha <joshguha@gmail.com>
2020-08-24 15:42:03 +0100

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,6 @@ export default function IndexPage() {
       <h2>
         I'm Josh, a full-stack developer living in the beautiful Birmingham, UK.
       </h2>
-      <p>
-        Need a developer? <Link to="/contact">Contact me</Link>
-      </p>
     </Layout>
   )
 }
